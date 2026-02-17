c3d6ba2a11529fe0de0df432ffab13e88c16aa6d
vitalik <vitalgreg1988@gmail.com>
2026-02-17 18:08:53 +0200

    add inngest files dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default async function Home() {
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
+            
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
---
+++ src/inngest/functions.ts
@@ -0,0 +1,11 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    //${event.data.email as string}
+    return { message: `Hello` };
+  },
+);
